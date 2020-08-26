c5bfa2cae1eda0fdb4de99f5d43c230973eb591e
simongarisch <gatman946@gmail.com>
2020-08-26 13:38:45 +1000

    adding template app to launch

---
+++ {{cookiecutter.package_name}}/main.py
@@ -0,0 +1,9 @@
+import {{ cookiecutter.package_name }}
+
+
+def main():
+    {{ cookiecutter.package_name }}.app.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/app.py
@@ -0,0 +1,18 @@
+import streamlit
+
+
+def run():
+    streamlit.markdown("""
+        # Welcome to streamlit
+
+        Here are some resources to help you get started:
+        -  [Streamlit site](https://www.streamlit.io/)
+        -  [Components demo app](https://fullstackstation.com/streamlit-components-demo)
+        -  [Tutorial](https://docs.streamlit.io/en/latest/tutorial/create_a_data_explorer_app.html)
+        -  [Youtube](https://www.youtube.com/watch?v=R2nr1uZ8ffc)
+        -  [Awesome streamlit](https://github.com/MarcSkovMadsen/awesome-streamlit)
+    """)
+
+
+if __name__ == "__main__":
+    run()
